be85be910ad242d3c41d84d8a1f1259018c23b9e
Craig Nelson <cn60@hotmail.com>
2025-11-12 16:16:47 -0600

    Tweak UI title

---
+++ Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  StraightPool
+//
+//  Created by Craig  on 11/12/25.
+//
+
+import Foundation
---
+++ RootView.swift
@@ -0,0 +1,8 @@
+//
+//  RootView.swift
+//  StraightPool
+//
+//  Created by Craig  on 11/12/25.
+//
+
+import Foundation
